475b0195b767043cc4dbac4c99296a6da8cb8acf
malekbenz <malekbenz@gmail.com>
2018-04-22 11:48:59 +0100

    Add parasm

--- index.js
+++ index.js
@@ -1,54 +1,69 @@
+
 var path = require("path"),
     fs = require('fs'),
     utils = require('./utils');
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+    { name: 'verbose', alias: 'v', type: Boolean },
+    { name: 'src', alias: 's', type: String },
+    { name: 'dest', alias: 'd', type: String },
+    { name: 'day', type: Boolean },
+    { name: 'week', type: Boolean },
+    { name: 'month', type: Boolean }
+];
+
+const options = commandLineArgs(optionDefinitions)
+
+console.log('options', options);
+
+// var url = process.argv[2] || 'https://vs1.someurl/015.mp4';
+// var periode = 'month';
+// var distPath = process.argv[3] || __dirname;
+
+// if (distPath == __dirname) {
+//     distPath = path.join(distPath, utils.dirnameFromUrl(url));
+
+// }
+
+// if (!fs.existsSync(distPath)) {
+//     fs.mkdirSync(distPath);
+// }
 
 
-var url = process.argv[2] || 'https://vs1.someurl/015.mp4';
-var distPath = process.argv[3] || __dirname;
+// function extractPath(url, next) {
 
-if (distPath == __dirname) {
-    distPath = path.join(distPath, utils.dirnameFromUrl(url));
-    
-}
+//     var ext = path.extname(url);
+//     var fileName = path.basename(url, ext);
+//     var url = path.dirname(url);
 
-if (!fs.existsSync(distPath)) {
-    fs.mkdirSync(distPath);
-}
+//     if (next) {
+//         var url2 = url + '/' + utils.getNext(fileName) + ext;
+//         var filePath = path.join(distPath, utils.getNext(fileName) + ext);
+//         return {
+//             url: url2,
+//             filePath: filePath
+//         }
 
+//     }
+//     var url2 = url + '/' + fileName + ext;
+//     var filePath = path.join(distPath, fileName + ext);
+//     return {
+//         url: url2,
+//         filePath: filePath
+//     }
+// }
 
-function extractPath(url, next) {
+// currentFile = extractPath(url)
 
-    var ext = path.extname(url);
-    var fileName = path.basename(url, ext);
-    var url = path.dirname(url);
+// utils.download(currentFile.url, currentFile.filePath, fileDownload);
 
-    if (next) {
-        var url2 = url + '/' + utils.getNext(fileName) + ext;
-        var filePath = path.join(distPath, utils.getNext(fileName) + ext);
-        return {
-            url: url2,
-            filePath: filePath
-        }
+// function fileDownload(url, filePath) {
 
-    }
-    var url2 = url + '/' + fileName + ext;
-    var filePath = path.join(distPath, fileName + ext);
-    return {
-        url: url2,
-        filePath: filePath
-    }
-}
+//     if (fs.statSync(filePath).size > 5000) {
+//         var file = extractPath(url, true);
+//         utils.download(file.url, file.filePath, fileDownload);
+//     } else
+//         fs.unlinkSync(filePath);
 
-currentFile = extractPath(url)
-
-utils.download(currentFile.url, currentFile.filePath, fileDownload);
-
-function fileDownload(url, filePath) {
-
-    if (fs.statSync(filePath).size > 5000) {
-        var file = extractPath(url, true);
-        utils.download(file.url, file.filePath, fileDownload);
-    } else
-        fs.unlinkSync(filePath);
-
-}
+// }
